6fd29570be55672686902c8fca0869cef0b3a17f
gurinalex91 <gurinalex91@gmail.com>
2020-09-07 00:58:28 +0300

    Init Repo

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('.header_burger').click(function(event) {
+    $('.header_burger, .header_menu') .toggleClass('active');
+    $('body') .toggleClass('lock');
+  });
+});
+$(window).scroll(testScroll);
+var viewed = false;
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function testScroll() {
+  if (isScrolledIntoView($(".counter")) && !viewed) {
+      viewed = true;
+      $('.cntr').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+    });
+  }
+}
